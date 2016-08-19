9e0dab1cf0dd739f32c30f0caae6a20eb59c4482
rishabhk91 <rishabh.katoch0@gmail.com>
2016-08-19 19:19:50 +0530

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,119 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ksSwiper'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout, $stateParams) {
+    //Used to name the .html file
+
+    console.log("Testing Consoles");
+
+    $scope.template = TemplateService.changecontent("home");
+    $scope.menutitle = NavigationService.makeactive("Home");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.gallery = [{
+        image: "img/sec5/lunchfun.png",
+        name: "Lunch Funday"
+    }, {
+        image: "img/sec5/artattack.png",
+        name: "Art Attack"
+    }, {
+        image: "img/sec5/playday.png",
+        name: "Play Day"
+    }, {
+        image: "img/sec5/lego.png",
+        name: "Lego Lessons"
+    }, {
+        image: "img/sec5/lego.png",
+        name: "Lego Lessons"
+    }, {
+        image: "img/sec5/lego.png",
+        name: "Lego Lessons"
+    }];
+    $scope.section = {
+        one: "views/section/section1.html",
+        two: "views/section/section2.html",
+        three: "views/section/section3.html",
+        four: "views/section/section4.html",
+        five: "views/section/section5.html",
+        six: "views/section/section6.html"
+    };
+    $scope.changeFullPage = function(no) {
+        console.log(no);
+        $.fn.fullpage.moveTo(no);
+    };
+    $scope.$on('$viewContentLoaded', function() {
+        $timeout(function() {
+            $('.fullpage').fullpage();
+            // $('#scene').parallax();
+            console.log($stateParams.name);
+            $scope.homeval = $stateParams.name;
+            switch ($scope.homeval) {
+                case "contact":
+                    $.fn.fullpage.moveTo(7);
+                    break;
+                case "gallery":
+                    $.fn.fullpage.moveTo(6);
+                    break;
+                case "environment":
+                    $.fn.fullpage.moveTo(5);
+                    break;
+                case "ourprogrammes":
+                    $.fn.fullpage.moveTo(4);
+                    break;
+                case "ourteam":
+                    $.fn.fullpage.moveTo(3);
+                    break;
+                case "ourstory":
+                    $.fn.fullpage.moveTo(2);
+                    break;
+                case "home":
+                    $.fn.fullpage.moveTo(1);
+                    break;
+                default:
+                    $.fn.fullpage.moveTo(1);
+                    break;
+            }
+        }, 1000);
+    });
+
+
+    // $scope.mySlides = [
+    //   'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    //   'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    //   'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    //   'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+    // ];
+})
+
+.controller('headerctrl', function($scope, TemplateService) {
+    $scope.template = TemplateService;
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $(window).scrollTop(0);
+    });
+    $.fancybox.close(true);
+})
+
+.controller('languageCtrl', function($scope, TemplateService, $translate, $rootScope) {
+
+    $scope.changeLanguage = function() {
+        console.log("Language CLicked");
+
+        if (!$.jStorage.get("language")) {
+            $translate.use("hi");
+            $.jStorage.set("language", "hi");
+        } else {
+            if ($.jStorage.get("language") == "en") {
+                $translate.use("hi");
+                $.jStorage.set("language", "hi");
+            } else {
+                $translate.use("en");
+                $.jStorage.set("language", "en");
+            }
+        }
+        //  $rootScope.$apply();
+    };
+
+
+})
+
+;
---
+++ js/navigation.js
@@ -0,0 +1,81 @@
+var adminURL = "";
+if(isproduction)
+{
+  adminURL =  "http://www.wohlig.co.in/demo/index.php";
+}
+else {
+  adminURL = "http://localhost/demo/index.php";
+}
+
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function() {
+  // var navigation = [{
+  //   name: "Home",
+  //   classis: "active",
+  //   anchor: "home",
+  //   subnav: [{
+  //     name: "Subnav1",
+  //     classis: "active",
+  //     anchor: "home"
+  //   }]
+  //   name: "Our Story",
+  //   classics: "active",
+  //   anchor: "ourstory"
+  // }];
+
+  var navigation = [{
+    name: "Our Story",
+    classis: "active",
+    link: "ourstory",
+    fullno: 2,
+    subnav: []
+  },{
+    name: "Our Team",
+    classis: "active",
+    link: "ourteam",
+    fullno: 3,
+    subnav: []
+  },{
+    name: "Our Programmes",
+    classis: "active",
+    link: "ourprogrammes",
+    fullno: 4,
+    subnav: []
+  },{
+    name: "Environment",
+    classis: "active",
+    link: "environment",
+    fullno: 5,
+    subnav: []
+  },{
+    name: "Gallery",
+    classis: "active",
+    link: "gallery",
+    fullno: 6,
+    subnav: []
+  },{
+    name: "Contact Us",
+    classis: "active",
+    link: "contact",
+    fullno: 7,
+    subnav: []
+  }];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+
+  };
+});
