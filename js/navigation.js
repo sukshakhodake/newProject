d5ca0da9a0ba9c882bc2ba286fd2a262604d9340
sagarssabat <sagarssabat@gmail.com>
2016-11-21 16:26:17 +0530

    gallery change for slider done

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,9 @@
 var adminurl = "http://104.155.129.33:91/";
+var adminurl = "http://192.168.100.120:1337/";
 
 var navigationservice = angular.module('navigationservice', [])
 
-.factory('NavigationService', function($http) {
+.factory('NavigationService', function ($http) {
   // var navigation = [{
   //   name: "Home",
   //   classis: "active",
@@ -23,31 +24,31 @@ var navigationservice = angular.module('navigationservice', [])
     link: "ourstory",
     fullno: 2,
     subnav: []
-  },{
+  }, {
     name: "Our Team",
     classis: "active",
     link: "ourteam",
     fullno: 3,
     subnav: []
-  },{
+  }, {
     name: "Our Programmes",
     classis: "active",
     link: "ourprogrammes",
     fullno: 4,
     subnav: []
-  },{
+  }, {
     name: "Environment",
     classis: "active",
     link: "environment",
     fullno: 5,
     subnav: []
-  },{
+  }, {
     name: "Gallery",
     classis: "active",
     link: "gallery",
     fullno: 6,
     subnav: []
-  },{
+  }, {
     name: "Contact Us",
     classis: "active",
     link: "contact",
@@ -56,10 +57,10 @@ var navigationservice = angular.module('navigationservice', [])
   }];
 
   return {
-    getnav: function() {
+    getnav: function () {
       return navigation;
     },
-    makeactive: function(menuname) {
+    makeactive: function (menuname) {
       for (var i = 0; i < navigation.length; i++) {
         if (navigation[i].name == menuname) {
           navigation[i].classis = "active";
@@ -69,58 +70,58 @@ var navigationservice = angular.module('navigationservice', [])
       }
       return menuname;
     },
-  //   submitForm: function(mydata, callback) {
-  //   $http({
-  //     url: 'http://www.weaverspreschool.com/mail.php?name='+mydata.name+'&email='+mydata.email+'&mobile='+mydata.mobile+'&message='+mydata.message+'&subject='+mydata.subject,
-  //     method: 'GET',
-  //     withCredentials: true,
-  //     data: mydata
-  //   }).success(callback);
-  // },
-  submitContact: function(mydata, callback) {
-    console.log(mydata);
-    $http({
-      url: adminurl + 'Contact/saveContact',
-      method: 'POST',
-      withCredentials: true,
-      data: mydata
-    }).success(callback);
-},
-  getAboutUs: function(callback) {
+    //   submitForm: function(mydata, callback) {
+    //   $http({
+    //     url: 'http://www.weaverspreschool.com/mail.php?name='+mydata.name+'&email='+mydata.email+'&mobile='+mydata.mobile+'&message='+mydata.message+'&subject='+mydata.subject,
+    //     method: 'GET',
+    //     withCredentials: true,
+    //     data: mydata
+    //   }).success(callback);
+    // },
+    submitContact: function (mydata, callback) {
+      console.log(mydata);
+      $http({
+        url: adminurl + 'Contact/saveContact',
+        method: 'POST',
+        withCredentials: true,
+        data: mydata
+      }).success(callback);
+    },
+    getAboutUs: function (callback) {
       $http({
         url: adminurl + 'AboutUs/search',
         method: 'POST',
         withCredentials: true
       }).success(callback);
     },
-    getOurTeam: function(callback) {
-        $http({
-          url: adminurl + 'Team/search',
-          method: 'POST',
-          withCredentials: true
-        }).success(callback);
-      },
-      getOurProgrammes:function(callback) {
-          $http({
-            url: adminurl + 'Program/search',
-            method: 'POST',
-            withCredentials: true
-          }).success(callback);
-        },
-        getOurApproach:function(callback) {
-            $http({
-              url: adminurl + 'Approach/search',
-              method: 'POST',
-              withCredentials: true
-            }).success(callback);
-          },
-        getGallery:function(callback) {
-            $http({
-              url: adminurl + 'Gallery/getAllImages',
-              method: 'POST',
-              withCredentials: true
-            }).success(callback);
-          },
+    getOurTeam: function (callback) {
+      $http({
+        url: adminurl + 'Team/search',
+        method: 'POST',
+        withCredentials: true
+      }).success(callback);
+    },
+    getOurProgrammes: function (callback) {
+      $http({
+        url: adminurl + 'Program/search',
+        method: 'POST',
+        withCredentials: true
+      }).success(callback);
+    },
+    getOurApproach: function (callback) {
+      $http({
+        url: adminurl + 'Approach/search',
+        method: 'POST',
+        withCredentials: true
+      }).success(callback);
+    },
+    getGallery: function (callback) {
+      $http({
+        url: adminurl + 'Gallery/getAllImages',
+        method: 'POST',
+        withCredentials: true
+      }).success(callback);
+    },
 
 
   };
